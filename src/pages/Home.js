5b9a5ee11cd827ffa0017abeead4647b39e2a745
Benjamin Higginbotham <benridesbikes@Benjamins-MacBook-Air.local>
2021-01-03 19:26:51 -0500

    add side bar and background image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Home = (props) => {
+  return (
+    <article className="home">
+      <h3 className="banner-header">
+        Sign Up or Log In to Create Creepy Costumes!
+      </h3>
+    </article>
+  );
+};
 
-    return (
-        <>
-           <h3 className="banner-header">Sign Up or Log In to Create Creepy Costumes!</h3>
-        </>
-    )
-}
-
-export default Home
+export default Home;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,65 +1,74 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {GlobalContext} from '../App';
+import React from "react";
+import { Link } from "react-router-dom";
+import { GlobalContext } from "../App";
 
 const Login = (props) => {
-    //Deconstruct globalState and setGlobalState and pass into useContext
-    const { globalState, setGlobalState } = React.useContext(GlobalContext);
-    const { url } = globalState;
+  //Deconstruct globalState and setGlobalState and pass into useContext
+  const { globalState, setGlobalState } = React.useContext(GlobalContext);
+  const { url } = globalState;
 
-    //create empty user object
-    const blank = {
-        username: '',
-        password: ''
-    };
+  //create empty user object
+  const blank = {
+    username: "",
+    password: "",
+  };
 
-    //Define state and setState for form 
-    const [form, setForm] = React.useState(blank);
+  //Define state and setState for form
+  const [form, setForm] = React.useState(blank);
 
-    //Allow user to type into text field
-    const handleChange = (event) => {
-        setForm({...form, [event.target.name]: event.target.value})
-    }
+  //Allow user to type into text field
+  const handleChange = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
 
-    //When user clicks Log in
-    const handleSubmit = (event) => {
-        //stop page from reloading
-        event.preventDefault();
-        //deconstruct username and password from state
-        const { username, password } = form
-        //make API call
-        fetch(`${url}/auth/login`, {
-            //enter method details
-            method: "post",
-            headers: {
-            "Content-Type": "application/json"
-            },
-            body: JSON.stringify({username, password})
-            })
-        //convert response to json
-        .then(response => response.json())
-        .then(data => {
-            //store token from logged-in user in local storage
-            window.localStorage.setItem("token", JSON.stringify(data));
-            //add token from logged-in user to globalState
-            setGlobalState({...globalState, token: data.token});
-            //reset the form
-            setForm(blank)
-            //send user to home page
-            props.history.push('/');
-        });
+  //When user clicks Log in
+  const handleSubmit = (event) => {
+    //stop page from reloading
+    event.preventDefault();
+    //deconstruct username and password from state
+    const { username, password } = form;
+    //make API call
+    fetch(`${url}/auth/login`, {
+      //enter method details
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      //convert response to json
+      .then((response) => response.json())
+      .then((data) => {
+        //store token from logged-in user in local storage
+        window.localStorage.setItem("token", JSON.stringify(data));
+        //add token from logged-in user to globalState
+        setGlobalState({ ...globalState, token: data.token });
+        //reset the form
+        setForm(blank);
+        //send user to home page
+        props.history.push("/");
+      });
+  };
 
-    }
-    
-    return (
-        <>
-            <form onSubmit={handleSubmit} className="banner-header">
-                <input type="text" name="username" value={form.username} onChange={handleChange} />
-                <input type="password" name="password" value={form.password} onChange={handleChange} />
-                <input type="submit" value="Log In" className="submit-button" />
-            </form>
-        </>
-    )
-}
+  return (
+    <article className="form">
+      <form onSubmit={handleSubmit} className="banner-header">
+        <input
+          type="text"
+          name="username"
+          value={form.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={form.password}
+          onChange={handleChange}
+        />
+        <input type="submit" value="Log In" className="submit-button" />
+      </form>
+    </article>
+  );
+};
 
-export default Login
+export default Login;
