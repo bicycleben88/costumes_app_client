09b39ae3f72d3e87df0baf66fe1888c9d0a32716
Benjamin Higginbotham <benridesbikes@Benjamins-MacBook-Air.local>
2021-01-05 20:38:50 -0500

    add responsive desgin

--- src/components/Costume.js
+++ src/components/Costume.js
@@ -4,27 +4,24 @@ const Costume = (props) => {
   const { costume } = props;
   return (
     <div className="costume">
-      <h4>{costume.name}</h4>
-      <div>
-        {costume.accessory ? (
-          <div
-            className="item"
-            style={{ backgroundImage: `url(${costume.accessory.img})` }}
-          />
-        ) : null}
-        {costume.top ? (
-          <div
-            className="item"
-            style={{ backgroundImage: `url(${costume.top.img})` }}
-          />
-        ) : null}
-        {costume.bottom ? (
-          <div
-            className="item"
-            style={{ backgroundImage: `url(${costume.bottom.img})` }}
-          />
-        ) : null}
-      </div>
+      {costume.accessory ? (
+        <div
+          className="item"
+          style={{ backgroundImage: `url(${costume.accessory.img})` }}
+        />
+      ) : null}
+      {costume.top ? (
+        <div
+          className="item"
+          style={{ backgroundImage: `url(${costume.top.img})` }}
+        />
+      ) : null}
+      {costume.bottom ? (
+        <div
+          className="item"
+          style={{ backgroundImage: `url(${costume.bottom.img})` }}
+        />
+      ) : null}
     </div>
   );
 };
