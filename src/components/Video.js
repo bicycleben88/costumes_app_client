6210f29be0bbdb9de0128b5d8b0fcf3d7b12c570
Ben Higginbotham <bicycleben88@gmail.com>
2021-03-22 10:51:32 -0400

    toggle play button

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,7 +2,9 @@ import React, { useRef } from "react";
 
 const Video = () => {
   const videoRef = useRef();
+  const playButtonRef = useRef();
   let video;
+  let playButton;
 
   const togglePlay = () => {
     if (video.paused) {
@@ -12,14 +14,33 @@ const Video = () => {
     }
   };
 
+  const updateButton = () => {
+    const icon = video.paused ? "►" : "❚ ❚";
+    playButton.textContent = icon;
+  };
+
   React.useEffect(() => {
     video = videoRef.current;
+    playButton = playButtonRef.current;
+    playButton.textContent = "►";
     video.src = "skeleton.mp4";
   }, []);
 
   return (
     <div id="video">
-      <video ref={videoRef} onClick={togglePlay} />
+      <video
+        ref={videoRef}
+        onClick={togglePlay}
+        onPlay={updateButton}
+        onPause={updateButton}
+      />
+      <div className="player_controls">
+        <button
+          ref={playButtonRef}
+          className="player_button"
+          onClick={togglePlay}
+        />
+      </div>
     </div>
   );
 };
