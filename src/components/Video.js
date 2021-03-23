1dfabe403bd9ade8b74b6c6e85577fedf56ff973
Ben Higginbotham <bicycleben88@gmail.com>
2021-03-23 11:24:52 -0400

    add progress bar

--- src/components/Video.js
+++ src/components/Video.js
@@ -3,6 +3,8 @@ import React, { useRef } from "react";
 const Video = () => {
   const videoRef = useRef();
   const playButtonRef = useRef();
+  const progressBarRef = useRef();
+  let progressBar;
   let video;
   let playButton;
 
@@ -23,9 +25,15 @@ const Video = () => {
     video.currentTime += parseFloat(e.target.dataset.skip);
   };
 
+  const handleProgress = () => {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+  };
+
   React.useEffect(() => {
     video = videoRef.current;
     playButton = playButtonRef.current;
+    progressBar = progressBarRef.current;
     playButton.textContent = "►";
     video.src = "skeleton.mp4";
   }, []);
@@ -37,12 +45,34 @@ const Video = () => {
         onClick={togglePlay}
         onPlay={updateButton}
         onPause={updateButton}
+        onTimeUpdate={handleProgress}
       />
       <div className="player-controls">
+        <div className="progress">
+          <div className="progress-filled" ref={progressBarRef} />
+        </div>
         <button
           ref={playButtonRef}
           className="player-button"
           onClick={togglePlay}
+        />
+        <input
+          type="range"
+          className="slider"
+          name="volume"
+          min="0"
+          max="1"
+          step="0.05"
+          value="1"
+        />
+        <input
+          type="range"
+          className="slider"
+          name="playbackRate"
+          min="0.5"
+          max="2"
+          step="0.1"
+          value="1"
         />
         <button data-skip="-10" className="player-button" onClick={skip}>
           « 10s{" "}
